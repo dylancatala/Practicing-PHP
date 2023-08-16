e9b34e04253449e88d14559c3054b10bb1908c2b
dylancatala <dylan.catala@proton.me>
2023-08-16 22:35:01 +0200

    Adding variables

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
-<?php
+<!-- <?php
 	echo "hello php";
 ?>
 
-
-
 <?php 
-  echo "This is my 1st paragraph"
+  echo "This is my 1st paragraph";
 ?>
 
 <?php
-  echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque hic quam, explicabo laboriosam, asperiores, ad ut vero a harum soluta quisquam debitis beatae voluptates aliquid ducimus expedita laudantium architecto modi?"
+  echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque hic quam, explicabo laboriosam, asperiores, ad ut vero a harum soluta quisquam debitis beatae voluptates aliquid ducimus expedita laudantium architecto modi?";
 ?>
 
-
 <?php 
-  echo "Ending of the test"
+  echo "Ending of the test";
+?> -->
+
+
+
+<!-- Variables  -->
+
+<!-- 1st Method w/ double quotes and space -->
+<?php 
+$greeting = "Hello";
+  echo $greeting . " " . "Everybody!";
+?>
+
+<!-- Best method  -->
+<?php 
+$country = "France";
+  echo "$country is a country!";
 ?>
