9eb9b11832a65096d70e43b1b65590f17462ed07
dylancatala <dylan.catala@proton.me>
2023-08-16 20:21:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	echo "hello php";
+?>
+
+
+
+<?php 
+  echo "This is my 1st paragraph"
+?>
+
+<?php
+  echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque hic quam, explicabo laboriosam, asperiores, ad ut vero a harum soluta quisquam debitis beatae voluptates aliquid ducimus expedita laudantium architecto modi?"
+?>
+
+
+<?php 
+  echo "Ending of the test"
+?>
