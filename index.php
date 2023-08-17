a207e7dda8122bb4bc47b55163eb148cd89f7ce0
dylancatala <dylan.catala@proton.me>
2023-08-17 12:07:18 +0200

    Arrays, foreach

--- index.php
+++ index.php
@@ -1,31 +1,78 @@
 <!-- <?php
-	echo "hello php";
-?>
+      echo "hello php";
+      ?>
 
-<?php 
-  echo "This is my 1st paragraph";
+<?php
+echo "This is my 1st paragraph";
 ?>
 
 <?php
-  echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque hic quam, explicabo laboriosam, asperiores, ad ut vero a harum soluta quisquam debitis beatae voluptates aliquid ducimus expedita laudantium architecto modi?";
+echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque hic quam, explicabo laboriosam, asperiores, ad ut vero a harum soluta quisquam debitis beatae voluptates aliquid ducimus expedita laudantium architecto modi?";
 ?>
 
-<?php 
-  echo "Ending of the test";
+<?php
+echo "Ending of the test";
 ?> -->
 
 
 
-<!-- Variables  -->
+<!-- VARIABLES  -->
 
 <!-- 1st Method w/ double quotes and space -->
-<?php 
+<?php
 $greeting = "Hello";
-  echo $greeting . " " . "Everybody!";
+echo $greeting . " " . "Everybody!";
 ?>
 
 <!-- Best method  -->
-<?php 
+<?php
 $country = "France";
-  echo "$country is a country!";
+echo "$country is a country!";
 ?>
+
+
+
+<!-- CONDITIONS -->
+<?php
+$name = "Blossoming flowers";
+$read = "true";
+
+if ($read) {
+  $message = "You have read $name";
+}
+?>
+
+<?php echo $message; ?>
+
+
+
+<!-- ARRAY, FOREACH -->
+
+<?php
+$books =  [
+  "Blue book",
+  "Bouggy book"
+];
+?>
+
+<?php foreach ($books as $book) : ?>
+  <?php echo $book ?>
+<?php endforeach; ?>
+
+
+<?php
+$animals = [
+  [
+    'name' => 'Blue',
+    'color' => 'Gray'
+  ],
+  [
+    'name' => 'Bouggy',
+    'color' => 'Gray/White'
+  ]
+];
+?>
+
+<?php foreach ($animals as $animal) : ?>
+  <?= $animal['name']; ?>
+<?php endforeach; ?>
